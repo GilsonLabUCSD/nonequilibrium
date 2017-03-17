0a21b967d6b1a9c3bd6d430735fbfb893edc3f9b
David Slochower <slochower@gmail.com>
2017-03-17 15:33:23 -0700

    Return figure object

--- figures.py
+++ figures.py
@@ -14,7 +14,7 @@ this.name = 'chi2THR175'  # Name of torsion
 this.cSubstrate = 10**-3  # Substrate concentration in [M]
 this.simulate()
 
-plot_input(this)
+fig = plot_input(this)
 ax = plt.gca()
 names=['Apo', 'Bound']
 ax.legend(names, frameon=True, loc='upper right', edgecolor='k', framealpha=1.0)
@@ -24,14 +24,14 @@ panel_label('b', panel_xoffset=-0.23, panel_yoffset=1.0)
 plt.savefig(directory + '1b.png', dpi=fig.dpi)
 plt.savefig(directory + '1b.pdf')
 
-plot_energy(this)
+fig = plot_energy(this)
 ax = plt.gca()
 ax.set_ylabel('Free energy (kcal mol$^{{-1}}$)')
 panel_label('c', panel_xoffset=-0.2, panel_yoffset=1.0)
 plt.savefig(directory + '1c.png', dpi=fig.dpi)
 plt.savefig(directory + '1c.pdf')
 
-plot_flux(this)
+fig = plot_flux(this)
 ax = plt.gca()
 ax.set_ylim([-200, 50])
 panel_label('d', panel_xoffset=-0.23, panel_yoffset=1.0)
